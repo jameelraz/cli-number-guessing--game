9070e297db1f03eb617e9b1c1a8313007b40042f
jameel raza <166707907+jameelraz@users.noreply.github.com>
2024-04-21 10:24:49 +0500

    a cli base number guessing game
    number guessing made with typescript node js and inquirer

---
+++ index.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer"
+
+const randomNumber = Math.floor(Math.random() *6 +1);
+
+
+const answres = await inquirer.prompt([
+  {
+    name : "userGuessNumber",
+    type : "number",
+    message :"please guess a number between 1 to 6"
+  } 
+]);
+
+if(answres.userGuessNumber=== randomNumber){
+    console.log("congratulations you guessed right number")
+}else {
+    console.log("opps! you guessed a wrong number")
+};
+ 
